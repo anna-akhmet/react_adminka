4a12ac05a34b9932702c2f41123b1bbc228e2a00
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-03-08 12:23:08 +0300

    Add new files with code from my old project

---
+++ src/Shop.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import Item from "./Item.js";
+import uuid from "react-uuid";
+
+export default function Shop() {
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const id = uuid();
+  console.log(items);
+
+  function handleAddItem(e) {
+    e.preventDefault();
+    if (name && desc) {
+      setItems([...items, { id: id, name: name, desc: desc }]);
+      setName("");
+      setDesc("");
+    }
+  }
+
+  function handleDeleteItem(e) {
+    setItems(items.filter((item) => item.id !== e.target.parentElement.id));
+  }
+
+  const itemRendered = items.map((item, index) => {
+    return (
+      <li className="ui-item-list" key={items[index].id} id={items[index].id}>
+        <Item info={items[index]} />
+        <button className="item-button" onClick={handleDeleteItem}>
+          Удалить
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <form onSubmit={handleAddItem}>
+        <div>
+          <label htmlFor="item-name">Введите товар:</label>
+          <input
+            id="item-name"
+            type="text"
+            placeholder="Название товара"
+            className="ui-textfield"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="item-desc">Введите описание:</label>
+          <input
+            id="item-desc"
+            type="text"
+            placeholder="Описание товара"
+            className="ui-textfield"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        <div className="form-footer">
+          {(!name || !desc) && (
+            <div className="validation">Заполните все поля</div>
+          )}
+          <input type="submit" className="ui-button" value="Добавить" />
+        </div>
+      </form>
+
+      <div>
+        {items.length === 0 && (
+          <p className="ui-title">Добавьте первый товар</p>
+        )}
+      </div>
+
+      <ul className="ui-list">{itemRendered}</ul>
+    </>
+  );
+}
