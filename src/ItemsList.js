06c3f33e74522e39ce9ef527fbb7baa7f63a1abc
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-03-29 15:31:17 +0300

    Transfer ul tag from Shop to ItemsList

--- src/ItemsList.js
+++ src/ItemsList.js
@@ -5,12 +5,14 @@ export default function ItemsList(props) {
 
   const itemsToRender = props.items.map((item, index) => {
     return (
-      <li className="ui-item-list" key={props.items[index].id} id={props.items[index].id}>
-        <Item info={props.items[index]} />
-        <button className="item-button" onClick={props.onDeleteItem}>
-          Удалить
-        </button>
-      </li>
+      <ul className="ui-list">
+        <li className="ui-item-list" key={props.items[index].id} id={props.items[index].id}>
+          <Item info={props.items[index]} />
+          <button className="item-button" onClick={props.onDeleteItem}>
+            Удалить
+          </button>
+        </li>
+      </ul>
     );
   });
 
