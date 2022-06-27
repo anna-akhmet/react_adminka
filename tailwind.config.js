16d5dc9bb7f6bbe0012468b52f8f8408e7c42f1e
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-06-27 15:49:57 +0300

    Add some rules from index.css as theme keys

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     './src/*.js'
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      blue: '#0000ff',
+      green: '#5efc8d',
+      light_grey: '#cccccc',
+      black: 'black',
+      white: 'white'
+    },
+    fontFamily: {
+      sans: ['Helvetica', 'sans-serif']
+    }, 
+    boxShadow: {
+      btn: '1px 1px 1px rgba(0, 0, 0, 0.2)',
+      btn_active: '0 0px rgba(0, 0, 0, 0.2)'
+    }
+
+  }
 }
