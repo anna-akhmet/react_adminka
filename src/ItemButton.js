7b4c877470692c8d182a968677b65b5b14b999e4
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-07-05 14:14:07 +0300

    Add TW css to the button`s class

--- src/ItemButton.js
+++ src/ItemButton.js
@@ -3,10 +3,22 @@ import React from "react";
 export default function ItemButton({children, onDeleteItem}) {
     
     return (
-        <button className="item-button" onClick={onDeleteItem}>
+        <button className="
+            bg-white
+            text-black text-base
+            border border-lightGrey rounded-[3px]
+            py-[5px] px-[20px]
+            mx-[10px]
+            min-w-[50px]
+            cursor-poiner
+            shadow-base
+            duration-200 ease-out
+            active:translate-y-px active:shadow-none
+            hover:opacity-80
+            disabled:cursor-none disabled:opacity-40
+        " 
+        onClick={onDeleteItem}>
             {children}
         </button>
     )
 }
-
-// finish button props 
