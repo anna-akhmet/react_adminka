fc8c2a822ca306bd731d2ea98628eff14c0134b2
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-07-05 12:17:40 +0300

    Add TW styles to ul and li, add ItemButton component

--- src/ItemsList.js
+++ src/ItemsList.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Item from "./Item.js";
+import ItemButton from "./ItemButton.js";
 
 export default function ItemsList(props) {
 
   const itemsToRender = props.items.map((item, index) => {
     return (
-      <ul className="ui-list">
-        <li className="ui-item-list" key={props.items[index].id} id={props.items[index].id}>
+      <ul className="m-[10px]">
+        <li className="mb-[80px] m-[10px]" key={props.items[index].id} id={props.items[index].id}>
           <Item info={props.items[index]} />
           <button className="item-button" onClick={props.onDeleteItem}>
             Удалить
           </button>
+          <ItemButton children="Удалить" onDeleteItem={props.onDeleteItem}/>
         </li>
       </ul>
     );
