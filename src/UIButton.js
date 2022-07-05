55e4bc7c74241e7fddf95a8c630e172a08d2c4ee
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-07-05 12:16:39 +0300

    Finish Tailwind styles for UIButton

--- src/UIButton.js
+++ src/UIButton.js
@@ -3,28 +3,24 @@ import React from "react";
 export default function UIButton({value}) {
 
     return (
-        <>
-            <input 
-                type="submit" 
-                className="
-                    bg-blue
-                    text-white text-base
-                    border-0 rounded-[3px]
-                    py-[15px] px-[20px]
-                    m-[10px]
-                    min-w-[150px]
-                    cursor-pointer
-                    shadow
-                    duration-200 ease-out
-                    hover:opacity-80
-                    active:shadow-none active:translate-y-1px
-                    disabled:cursor-none disabled:opacity-40
-                " 
-                value={value}
-                />
-        </>
+        <input 
+            type="submit" 
+            className="
+                bg-blue
+                text-white text-base
+                border-0 rounded-[3px]
+                py-[15px] px-[20px]
+                m-[10px]
+                min-w-[150px]
+                cursor-pointer
+                shadow-base
+                duration-200 ease-out
+                hover:opacity-80
+                active:translate-y-px active:shadow-none
+                disabled:cursor-none disabled:opacity-40
+            " 
+            value={value}
+        />
     )
 
 }
-
-// fix shadow 
